216e8d31c504761833f3509ce232233e3008e514
Gianluca Zavan <gianlucagianluca18@gmail.com>
2021-07-05 09:17:11 +0200

    patch: swapfocus + super-s binding to use it

--- config.h
+++ config.h
@@ -74,6 +74,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_s,      swapfocus,      {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
